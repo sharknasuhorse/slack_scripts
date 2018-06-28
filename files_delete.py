7241913692a87d0ca45bcae7c6b17dbb925aac5b
sharknasuhorse <naka-ryo6459@nifty.com>
2018-06-28 11:06:02 +0900

    add

--- files_delete.py
+++ files_delete.py
@@ -1,16 +1,17 @@
+"""This is a test program."""
 import json
 import requests
 
 
-slack_api_token = ""
+SLACK_API_TOKEN = ""
 
-res = requests.get("https://slack.com/api/files.list?token=" + slack_api_token)
+res = requests.get("https://slack.com/api/files.list?token=" + SLACK_API_TOKEN)
 files_list = json.loads(res.text)
 files_list = files_list["files"]
 
 for file in files_list:
-  if file["user"] == "UA9NX5L3H":
-    print(file["id"])
-    res = requests.get("https://slack.com/api/files.delete?token=" + slack_api_token + "&file=" + file["id"])
-  else:
-    pass
+    if file["user"] == "UA9NX5L3H":
+        print(file["id"])
+        res = requests.get("https://slack.com/api/files.delete?token=" + SLACK_API_TOKEN + "&file=" + file["id"])
+    else:
+        pass
